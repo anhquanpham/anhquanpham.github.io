60102459ce128a6a8bb62e200992ac0d16f2fa7f
Pham Anh Quan <phamanhquanbill@gmail.com>
2025-06-13 23:14:54 -0400

    career day

--- src/data/talks.js
+++ src/data/talks.js
@@ -7,6 +7,13 @@ const data = [
     image: '/images/academic/talks/PREC-talk.png',
     description: 'Gave a talk on beachhead markets for tech startups during the Fall 2024 semester. Slides available upon request, subject to context.',
   },
+  {
+    title: 'How To Ace Your Internship And Get Return Offers | Career Readiness Series 2023-2024',
+    venue: 'VinUniversity, Vietnam',
+    date: 'May 2024',
+    image: '/images/academic/talks/return-offers.jpg',
+    description: 'Shared insights on securing competitive research internships through initiative, preparation, and strategic positioning, based on personal experience earning an AI research internship offer from A*STAR Singapore.',
+  },
 ];
 
 export default data;
